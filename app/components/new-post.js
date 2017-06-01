01d9e7fd0d648ba0ba67bc6fc4bd8524f565a8b8
Ilene Gorski and Starlynne Start <ilene+starlynne@>
2017-06-01 16:17:15 -0700

    finish contact page

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -2,16 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   addNewPost: false,
+
   actions: {
     postFormShow() {
       this.set('addNewPost', true);
     },
 
     savePost() {
+      var preview = []
+       var content = this.get('content')
+       var prep = content.split('');
+      for (var i = 0; i <= 45; i++){
+        preview.push(prep[i]);
+      }
+      console.log(preview)
+      preview = preview.join('');
       var params = {
         title: this.get('title'),
         content: this.get('content'),
-        // tags: this.get('tags'),
+        preview: preview,
         user: this.get('user'),
       };
       console.log('inside new-post' + params);
--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   content: DS.attr(),
+  preview: DS.attr(),
   user: DS.belongsTo('user', {async: true})
 });
